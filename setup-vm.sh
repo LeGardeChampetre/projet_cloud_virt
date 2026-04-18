36e01789f43f33b38789584580c37d42659ca6d1
TheMuffinMan <arthur.montier76@gmail.com>
2026-04-18 19:00:54 +0200

    first commit

---
+++ setup-vm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e 
+
+sudo apt update
+sudo apt upgrade -y
+
+#install docker
+sudo apt install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list
+
+sudo apt update
+sudo apt install -y docker-ce-cli containerd.io docker-compose-plugin
+
+sudo usermod -aG docker ubuntu
+
+echo "Installation terminée!"
